3b198cd09ea6553117c305fd3164ba2d3064b8bd
Alex <andrlyadov@yandex.ru>
2020-03-24 18:42:41 +0000

    fix some troubles with find in doc

--- Practica/1.py
+++ Practica/1.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import re
 from docx import Document
+from docx.opc.exceptions import PackageNotFoundError
 from sys import argv
+from os.path import isfile
+
 
 def find_all_templates(document):
     templates = set()
@@ -11,34 +16,51 @@ def find_all_templates(document):
     for table in document.tables:
         for row in table.rows:
             for cell in row.cells:
-                templates += find_all_templates(cell)
+                templates = templates.union(find_all_templates(cell))
                 #for match in re.search('\\{\\{.*?\\}\\}', cell):
                 #    templates.add(match.group(0))
-
     return templates
 
-def docx_replace_regex(document, regex, replace):
+
+def docx_replace(document, substr, replace):
     for p in document.paragraphs:
-        if regex.search(p.text):
+        if substr in p.text:
             inline = p.runs
             # Loop added to work with runs (strings with same style)
+            was_replaced = False
             for i in range(len(inline)):
-                if regex.search(inline[i].text):
-                    text = regex.sub(replace, inline[i].text)
+                if substr in inline[i].text:
+                    #text = regex.sub(replace, inline[i].text)
+                    text = inline[i].text.replace(substr, replace)
+                    was_replaced = True
+                    #print('replacing {} with {} in {}'.format(substr, replace, inline[i].text))
                     inline[i].text = text
-
+            if not was_replaced:
+                text = p.text.replace(substr, replace)
+                p.text = text
     for table in document.tables:
         for row in table.rows:
             for cell in row.cells:
-                docx_replace_regex(cell, regex , replace)
-
+                docx_replace(cell, substr , replace)
 
 
 def main():
-    filename = 'test.docx'
-    if len(argv) >= 2:
-        filename = argv[-1]
-    doc = Document(filename)
+    default_filename = '/home/nicto/Документы/document.docx'
+    filename = input("Enter your filename: ")
+    if filename == '':
+        print("The default document will be used.")
+        filename = default_filename
+
+        if len(argv) >= 2:
+            filename = argv[-1]
+    if not isfile(filename):
+        print("No such file: {}".format(filename))
+        return main()
+    try:
+        doc = Document(filename)
+    except PackageNotFoundError:
+        print("Invalid file format. You should use only \"docx\" ")
+        return main()
 
     replacements = {}
     for template in find_all_templates(doc):
@@ -46,9 +68,14 @@ def main():
         replacements[template] = replacement
 
     for template, replacement in replacements.items():
-        docx_replace_regex(doc, re.compile(template), replacement)
+        docx_replace(doc, template, replacement)
 
-    doc.save('test-output.docx')
+    doc.save('/home/nicto/Документы/document-output.docx')
+
 
 if __name__ == '__main__':
-    main()
+    while True:
+        try:
+            main()
+        except KeyboardInterrupt:
+            print("\nПродолжение работы")
